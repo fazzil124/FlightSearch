70127ca92eaa7e1ae17315586415066caa03455f
fazil124 <fazil124@gmail.com>
2015-02-25 07:24:46 +0000

    Second Edition

--- FlightSearch/WebContent/Script/Request.js
+++ FlightSearch/WebContent/Script/Request.js
@@ -1,39 +1,29 @@
+
 /**
  * 
  */
 
 var arr = {
-	"request" : {
-		"passengers" : {
-			"kind" : "qpxexpress#passengerCounts",
-			"adultCount" : "1",
-			"childCount" : "0",
-			"infantInLapCount" : "0",
-			"infantInSeatCount" : "0",
-			"seniorCount" : "0"
-		},
-		"slice" : [ {
-			"kind" : "qpxexpress#sliceInput",
-			"origin" : "DUB",
-			"destination" : "DXB",
-			"date" : "2015-03-05",
-			"maxStops" : "2",
-			"maxConnectionDuration" : "60",
-			"preferredCabin" : "COACH",
-			"permittedDepartureTime" : {
-				"kind" : "qpxexpress#timeOfDayRange",
-				"earliestTime" : "06:00",
-				"latestTime" : "23:00"
-			},
-			
-		} ],
-		"maxPrice" : "10000",
-		"saleCountry" : "EUR",
-		"refundable" : "false",
-		"solutions" : "500"
-	}
+		"request": {
+		    "slice": [
+		      {
+		        "origin": "DUB",
+		        "destination": "DXB",
+		        "date": "2015-02-28"
+		      }
+		    ],
+		    "passengers": {
+		      "adultCount": 1,
+		      "infantInLapCount": 0,
+		      "infantInSeatCount": 0,
+		      "childCount": 0,
+		      "seniorCount": 0
+		    },
+		    "solutions": 20,
+		    "refundable": false
+		  }
 };
-var select=("#flights-display");
+
 $.ajax({
 	url : 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDp146UsLz8R6rVv5a4TSsMzBZkIm5FX8o',
 	type : 'POST',
@@ -43,6 +33,7 @@ $.ajax({
 	async : false,
 	success : function(msg) {
 		var obj = JSON.parse(msg);
-		select.apend(obj.airport);
+		alert(obj.kind.requestId);
+		$('#flights-display').apend(obj);
 	}
 });
